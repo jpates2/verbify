c6b397db1501b680fbad17d7792f315c1840b722
Jess Pates <jesspates@hotmail.co.uk>
2023-10-31 16:59:35 +0000

    intro text added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,12 +4,18 @@ export default function Intro() {
   return (
     <section id="intro">
       <div className={classes["intro__container"]}>
-          <div className={classes["intro__box"]}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, velit sequi nemo, perferendis labore necessitatibus reprehenderit, dolore voluptatibus placeat numquam vel ex beatae obcaecati iusto amet eum! Autem nesciunt sequi adipisci doloremque cumque nemo, laborum iusto minus, temporibus magnam quo voluptatem, eaque inventore fugit obcaecati eos. Odio accusantium quas ab.
+        <div className={classes["intro__box"]}>
+          <p>
+            Welcome to Verbify, your ultimate resource for mastering Spanish verbs with ease and enjoyment.
+          </p>
+          <p>
+            Review any verb and its many conjugations, then it's time to get practising with our flashcards! Sign up now and create a profile to track your progress, build a streak, and work on those pesky problem verbs.
+          </p>
+          <p>
+            We're all about making learning fun and effective, so join us for an exciting journey towards mastering Spanish verbs!
+          </p>
         </div>
       </div>
-
-
     </section>
   )
 }
