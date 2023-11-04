1ad56540b4d5b71e6ca290df4480da54929f7684
Jess Pates <jesspates@hotmail.co.uk>
2023-11-04 23:13:19 +0000

    verb conditional output

--- src/verb/TenseGroup.js
+++ src/verb/TenseGroup.js
@@ -1,22 +1,76 @@
+import { useState } from "react";
 import VerbPair from "../verbs/VerbPair";
 import classes from "./TenseGroup.module.css";
 
+const verbExample = {
+  present: {
+    yo: "soy",
+    tu: "eres",
+    el: "es",
+    nos: "somos",
+    vos: "sois",
+    ellos: "son"
+  },
+  preterite: {
+    yo: "fui",
+    tu: "fuiste",
+    el: "fue",
+    nos: "fuimos",
+    vos: "fuisteis",
+    ellos: "fueron"
+  },
+  imperfect: {
+    yo: "era",
+    tu: "eras",
+    el: "era",
+    nos: "eramos",
+    vos: "erais",
+    ellos: "eran"
+  },
+  conditional: {
+    yo: "seria",
+    tu: "serias",
+    el: "seria",
+    nos: "seriamos",
+    vos: "seriais",
+    ellos: "serian"
+  },
+  future: {
+    yo: "sere",
+    tu: "seras",
+    el: "sera",
+    nos: "seremos",
+    vos: "sereis",
+    ellos: "seran"
+  }
+}
+
 export default function TenseGroup({ children, tenses }) {
+  const [verbTense, setVerbTense] = useState("present");
+  const selectedTense = verbExample[verbTense];
+
+  function handleVerbClick(tense) {
+    setVerbTense(tense);
+  }
+
   return (
     <section className={classes["tense-group__container"]}>
       <div className={classes["tense-group__header"]}>{children}</div>
       <ul className={classes["tense-group__list"]}>
         {tenses.map(tense => (
-          <li key={`${children} ${tense}`}>{tense}</li>
+          <li key={`${children} ${tense}`} >
+            <div onClick={() => {handleVerbClick(tense.toLowerCase())}}>{tense}</div>
+            {verbTense === tense.toLowerCase() && <div className={classes["tense__underline"]}></div>}
+          </li>
         ))}
       </ul>
       <div>
-        <VerbPair pronoun="yo" conVerb="soy" />
-        <VerbPair pronoun="tu" conVerb="eres" />
-        <VerbPair pronoun="el / ella / usted" conVerb="es" />
-        <VerbPair pronoun="nosotros / nosotras" conVerb="somos" />
-        <VerbPair pronoun="vosotros / vosotras" conVerb="sois" />
-        <VerbPair pronoun="ellos / ellas / ustedes" conVerb="son" />
+        <VerbPair pronoun="yo" conVerb={selectedTense.yo} />
+        <VerbPair pronoun="tu" conVerb={selectedTense.tu} />
+        <VerbPair pronoun="el / ella / usted" conVerb={selectedTense.el} />
+        <VerbPair pronoun="nosotros / nosotras" conVerb={selectedTense.nos} />
+        <VerbPair pronoun="vosotros / vosotras" conVerb={selectedTense.vos} />
+        <VerbPair pronoun="ellos / ellas / ustedes" conVerb={selectedTense.ellos} />
       </div>
     </section>
   )
