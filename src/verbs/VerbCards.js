05206cfd518ff0e93f959c5f5c77d06079bc7e83
Jess Pates <jesspates@hotmail.co.uk>
2023-11-05 21:46:21 +0000

    search results limited by screen size

--- src/verbs/VerbCards.js
+++ src/verbs/VerbCards.js
@@ -1,24 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./VerbCards.module.css";
 import { VerbInfo } from "../info/verb-info";
 import VerbCard from "./VerbCard";
 
 export default function VerbCards({ searchTerms }) {
+  let initialLimit;
+  if (window.innerWidth >= 1000) {initialLimit = 10}
+  if (window.innerWidth < 1000 && window.innerWidth > 600) {initialLimit = 8}
+  if (window.innerWidth <= 600) {initialLimit = 6}
+  const [limit, setLimit] = useState(initialLimit);
+
+  function handleLimitIncrease() {
+    setLimit((prevLimit) => {
+      return prevLimit + initialLimit;
+    })
+  }
+
   const filteredVerbs = VerbInfo.filter(verb => {
     return (verb.infinitive.includes(searchTerms) || verb.translation.includes(searchTerms));
   })
 
+  const verbResults = filteredVerbs.map(verb => (
+    <Link to={`/verbs/${verb.infinitive}`} key={verb.infinitive}>
+      <VerbCard
+        verb={verb.infinitive}
+        translation={verb.translation}
+      />
+    </Link>
+  ))
+
   return (
     <section>
       <div className={classes["verb-cards__container"]}>
-        {filteredVerbs.map(verb => (
-          <Link to={`/verbs/${verb.infinitive}`} key={verb.infinitive}>
-            <VerbCard
-              verb={verb.infinitive}
-              translation={verb.translation}
-            />
-          </Link>
-        ))}
+        {verbResults.slice(0, limit)}
+      </div>
+      <div className={classes["verb-cards__button-container"]}>
+        {limit < filteredVerbs.length && <button onClick={handleLimitIncrease} className={classes["verb-cards__add-button"]}>View More</button>}
       </div>
     </section>
   )
