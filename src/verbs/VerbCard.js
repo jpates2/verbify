38654f4b77618cc0b6dfe1b1123f68d1bd12ed7c
Jess Pates <jesspates@hotmail.co.uk>
2023-11-05 21:51:43 +0000

    framer motion fade in added to verb cards

--- src/verbs/VerbCard.js
+++ src/verbs/VerbCard.js
@@ -1,10 +1,17 @@
+import { motion } from "framer-motion";
 import classes from "./VerbCard.module.css";
 
 export default function VerbCard({ verb, translation }) {
   return (
-    <div className={classes["verb-card"]}>
+    <motion.div
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{duration: 1}}
+      exit={{opacity: 1}}
+      className={classes["verb-card"]}
+    >
       <div className={classes["verb-card__spanish"]}>{verb}</div>
       <div className={classes["verb-card__english"]}>{translation}</div>
-    </div>
+    </motion.div>
   )
 }
