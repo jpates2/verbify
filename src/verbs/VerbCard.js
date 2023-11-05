2c8c50d5e86f7fe9da8d733f729c83b9b1f6c952
Jess Pates <jesspates@hotmail.co.uk>
2023-11-05 21:59:58 +0000

    scale added to verb cards framer motion

--- src/verbs/VerbCard.js
+++ src/verbs/VerbCard.js
@@ -4,10 +4,10 @@ import classes from "./VerbCard.module.css";
 export default function VerbCard({ verb, translation }) {
   return (
     <motion.div
-      initial={{opacity: 0}}
-      animate={{opacity: 1}}
-      transition={{duration: 1}}
-      exit={{opacity: 1}}
+      initial={{opacity: 0, scale: 0.5}}
+      animate={{opacity: 1, scale: 1}}
+      transition={{duration: 0.5}}
+      exit={{opacity: 1, scale: 1}}
       className={classes["verb-card"]}
     >
       <div className={classes["verb-card__spanish"]}>{verb}</div>
