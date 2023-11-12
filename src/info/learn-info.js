c111418b8b11c604921945a04eb2f27ca1819646
Jess Pates <jesspates@hotmail.co.uk>
2023-11-12 21:46:37 +0000

    text added to home page

--- src/info/learn-info.js
+++ src/info/learn-info.js
@@ -5,26 +5,26 @@ import TimestableImg from "../assets/images/timestable.jpg";
 
 export const LearnInfo = [
   {
-    title: "Review",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At maxime numquam rem repellat non iure nostrum quo necessitatibus repellendus ullam.",
+    title: "Unlock the World of Verbs",
+    description: "Jump into our library for a thorough look at Spanish conjugations and tenses. Whether you're nailing down irregular verbs or just getting the hang of essential tenses, our verb library is your go-to for building a solid base in Spanish.",
     img: BooksImg,
-    link: "Visit Verb Bank"
+    link: "Visit Verb Library"
   },
   {
-    title: "Practice Makes Perfect",
-    description: "Velit odit asperiores quo in esse cum, sunt neque maxime reiciendis, dignissimos commodi suscipit provident sint perspiciatis! Necessitatibus eveniet numquam quos impedit facere.",
+    title: "Flashcards for Fluency",
+    description: "Get a grip on verb conjugations using our practical flashcards. We have a plentiful supply to guide you in mastering Spanish verbs until they become second nature.",
     img: PostItsImg,
     link: "Sign Up Now"
   },
   {
-    title: "A Little A day",
-    description: "In id quia modi voluptatum pariatur ipsam eum cumque? Cupiditate voluptatibus quia sapiente ipsum pariatur nobis hic nam culpa deleniti necessitatibus doloremque praesentium porro esse numquam ullam.",
+    title: "A Daily Dose of Spanish",
+    description: "Make Spanish part of your daily routine. Build your streak to see your progress day by day. Mastering Spanish becomes second nature when it's seamlessly integrated into your daily habits.",
     img: SignImg,
     link: "Start Today"
   },
   {
-    title: "Repetition",
-    description: "Excepturi modi necessitatibus porro atque vitae, eveniet assumenda ullam quod, earum eum nisi quidem eligendi quisquam reiciendis voluptatibus?",
+    title: "Your Progress, Your Way",
+    description: "Sign up and explore your personalized user profile – the central hub for your language progress. Track your streak, review previous scores, and receive tailored practice suggestions. It's all about advancing at your own pace, making Spanish learning uniquely tailored to you.",
     img: TimestableImg,
     link: "Start Your Streak Now"
   }
