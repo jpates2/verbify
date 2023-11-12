d8ab257a313ecf2891f345154017730715211420
Jess Pates <jesspates@hotmail.co.uk>
2023-11-12 15:28:18 +0000

    flashcard card css

--- src/flashcard/Card.js
+++ src/flashcard/Card.js
@@ -2,6 +2,8 @@ import classes from "./Card.module.css";
 
 const pronouns = ["yo", "tú", "él", "ella", "usted", "nosotros", "nosotras", "vosotros", "vosotras", "ellos", "ellas", "ustedes"];
 
+const accents = ["á", "é", "í", "ó", "ú", "ñ", "ü"];
+
 export default function Card({ tense, subtense }) {
   const pronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
 
@@ -14,8 +16,20 @@ export default function Card({ tense, subtense }) {
       </div>
       <div className={classes["card__verb-container"]}>
         <div className={classes["card__pronoun"]}>{pronoun}</div>
-        <div> &nbsp; &nbsp; &nbsp;</div>
+        <div> &nbsp; &nbsp;</div>
         <div className={classes["card__verb"]}>decir</div>
+      </div>
+      <form className={classes["card__form"]}>
+        <div className={classes["card__form-input"]}>
+          <input className={classes["card__answer"]} placeholder="answer" />
+          <div className={classes["card__answer-underline"]}></div>
+        </div>
+        <button className={classes["card__enter-button"]}>Enter</button>
+      </form>
+      <div className={classes["card__accent-container"]}>
+        {accents.map(accent => (
+          <span key={accent} className={classes["card__accent"]}>{accent}</span>
+        ))}
       </div>
 
     </div>
