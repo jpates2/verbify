5436d40a8b8f7d3637ce7f087823e267a66ba73f
Jess Pates <jesspates@hotmail.co.uk>
2023-11-14 21:48:13 +0000

    timer logic

--- src/flashcard/Timer.js
+++ src/flashcard/Timer.js
@@ -1,9 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import Context from "../store/context.js";
 import classes from "./Timer.module.css";
 
 export default function Timer() {
   const ctx = useContext(Context);
+  const ctxTime = ctx.timer * 60;
+  const [counter, setCounter] = useState(ctxTime)
+
+  function formatTime (time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const formattedSeconds = seconds.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    })
+    return `${minutes} : ${formattedSeconds}`
+  }
+
+  useEffect(() => {
+    let timer;
+    if (ctx.go === true && counter > 0) {
+      timer = setTimeout(() => setCounter(c => c - 1), 1000);
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [counter, ctx.go]);
 
   let timerClass;
   if (ctx.go === true) {
@@ -16,7 +41,7 @@ export default function Timer() {
   return (
     <div>
       <div className={classes["timer__title"]}>Timer</div>
-      <div className={timerClass}>{ctx.timer}</div>
+      <div className={timerClass}>{formatTime(counter)}</div>
     </div>
   )
 }
