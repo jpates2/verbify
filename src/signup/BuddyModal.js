150c9511f9eb6a63d71fca1d8606727307f9acf1
Jess Pates <jesspates@hotmail.co.uk>
2023-11-21 17:11:56 +0000

    modal css amends, learning buddy image added

--- src/signup/BuddyModal.js
+++ src/signup/BuddyModal.js
@@ -1,7 +1,12 @@
+import classes from "./BuddyModal.module.css";
+
 export default function BuddyModal() {
+
+
   return (
-    <div>
-      buddy
+    <div className={classes["buddy-modal__container"]}>
+      <div className={classes["buddy-modal__header"]}>Choose your learning buddy...</div>
+      <div></div>
     </div>
   )
 }
