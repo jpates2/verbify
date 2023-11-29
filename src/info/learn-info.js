f54034d7603e703364f8da297aa068374cbf6e8d
Jess Pates <jesspates@hotmail.co.uk>
2023-11-29 21:44:22 +0000

    learn card images replaced

--- src/info/learn-info.js
+++ src/info/learn-info.js
@@ -1,34 +1,34 @@
-import BooksImg from "../assets/images/books.jpg";
-import PostItsImg from "../assets/images/postits.jpg";
-import SignImg from "../assets/images/sign.jpg";
-import TimestableImg from "../assets/images/timestable.jpg";
+import Screenshot1 from "../assets/images/screenshot1.png";
+import Screenshot2 from "../assets/images/screenshot2.png";
+import Screenshot3 from "../assets/images/screenshot3.png";
+import Screenshot4 from "../assets/images/screenshot4.png";
 
 export const LearnInfo = [
   {
     title: "Unlock the World of Verbs",
     description: "Jump into our library for a thorough look at Spanish conjugations and tenses. Whether you're nailing down irregular verbs or just getting the hang of essential tenses, our verb library is your go-to for building a solid base in Spanish.",
-    img: BooksImg,
+    img: Screenshot2,
     link: "Visit Verb Library",
     to: "/verbs"
   },
   {
     title: "Flashcards for Fluency",
     description: "Get a grip on verb conjugations using our practical flashcards. We have a plentiful supply to guide you in mastering Spanish verbs until they become second nature.",
-    img: PostItsImg,
+    img: Screenshot4,
     link: "Go To Flashcards",
     to: "/flashcards"
   },
   {
     title: "A Daily Dose of Spanish",
     description: "Make Spanish part of your daily routine. Build your streak to see your progress day by day. Mastering Spanish becomes second nature when it's seamlessly integrated into your daily habits.",
-    img: SignImg,
+    img: Screenshot3,
     link: "Start Today",
     to: "/flashcards"
   },
   {
     title: "Your Progress, Your Way",
     description: "Sign up and explore your personalized user profile – the central hub for your language progress. Track your streak, review previous scores, and receive tailored practice suggestions. It's all about advancing at your own pace, making Spanish learning uniquely tailored to you.",
-    img: TimestableImg,
+    img: Screenshot1,
     link: "Sign Up Now",
     to: ""
   }
